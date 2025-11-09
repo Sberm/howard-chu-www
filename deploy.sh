39bae65242ac5e7b2f070f77e7eec739fa1c51e9
Howard Chu <howardchu95@gmail.com>
2025-11-09 12:45:24 -0800

    deploy.sh: Don't exit when docker stop can't find running instance
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -e
-docker stop -t 1 homepage
-docker rm homepage
+docker stop -t 1 homepage || true
+docker rm homepage || true
 docker build -t sberm/howard-chu-www .
 docker run -p 80:80 -p 443:443 -d --restart unless-stopped --name homepage \
     -v /etc/letsencrypt:/etc/letsencrypt:ro \
